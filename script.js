b7a7e37d6c3a989025fd804cd6ada4b60e024541
Santiago <santiagoguba@unisabana.edu.co>
2024-03-16 16:43:57 -0500

    js

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 async function traerInfo(){
-    let numQuotes = 5;
+    let numQuotes = 10;
     try{
         fetch("https://thesimpsonsquoteapi.glitch.me/quotes?count=10")
         .then(response => response.json())
         .then(data =>{
             console.log(data);
-
-            
                 for(let i = 0; i < numQuotes; i++) {
                     characterContainer.innerHTML += `
                         <li class="quote">
                             <p>${data[i].character ?? ""}</p>
                         </li>
                     `;
-                }
-            
-
-
-
-           
-
-
+                } 
         })
     }catch(error){
         alert(error);
     }
 }
+
